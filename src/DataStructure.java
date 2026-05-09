1781b7d28c2059c312631fbbfb1e0f876781fb08
Anselm Kongo <kongoa0303@students.bowiestate.edu>
2026-05-09 07:50:54 -0400

    Update DataStructure.java

--- src/DataStructure.java
+++ src/DataStructure.java
@@ -1,10 +1,15 @@
-package healthbench.structures;
-
-import healthbench.model.PatientRecord;
-
+// Common interface that every project data structure must implement.
 public interface DataStructure {
+
+    // Add a new PatientRecord to the structure.
     void insertRecord(PatientRecord record);
+
+    // Find a PatientRecord using its generated ID.
     PatientRecord searchRecord(String id);
+
+    // Remove a PatientRecord using its generated ID.
     boolean deleteRecord(String id);
+
+    // Return all records for display or benchmarking.
     PatientRecord[] traverseRecords();
 }
